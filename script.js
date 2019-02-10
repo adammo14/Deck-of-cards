60046f94d8c60a65d5e4dca773e7de5b92ccf9a7
Adam Chmielewski <adammo14@hotmail.com>
2019-02-10 13:36:27 +0000

    V2
    ...

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
 var cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
 var suits = ['Clubs', 'Spades', 'Hearts', 'Diamonds'];
 
+var mixer = [];
+
+var drawnCards = [];
+
+function deal(){
+  //if card element already exists in DOM, throw err
+  var myEle = document.querySelector('.card');
+  if(myEle){
+    console.log('Error, deck has already been dealt');
+  } else {
+    for (var i = 0; i < cards.length; i++) {
+      for (var j = 0; j < suits.length; j++) {
+        mixer.splice(0, 0, cards[i] + ' of ' + suits[j]);
+      }
+    }
+    for (var i = 0; i < mixer.length; i++) {
+      var card = document.createElement('div');
+      card.setAttribute('class', 'card');
+      card.appendChild(document.createTextNode(mixer[i]));
+      document.querySelector('.card-container').appendChild(card);
+    }
+    console.log('Cards dealt');
+  }
+}
+
 function shuffle(){
-  var i = cards.length;
+  var i = mixer.length;
   var j;
   var temp;
   while (--i > 0) {
     j = Math.floor(Math.random() * (i + 1));
-    temp = cards[j];
-    cards[j] = cards[i];
-    cards[i] = temp;
+    temp = mixer[j];
+    mixer[j] = mixer[i];
+    mixer[i] = temp;
   }
-  clearDeck();
-  deal();
+  x = document.querySelectorAll('.card');
+  for (var i = 0; i < mixer.length; i++) {
+    x[i].innerText = mixer[i];
+  }
   console.log('Shuffle executed');
 }
 
@@ -21,24 +48,54 @@ function clearDeck(){
   cardElem = document.querySelectorAll('.card');
   Array.prototype.forEach.call(cardElem, function(node) {
     node.parentNode.removeChild(node);
+    mixer.pop();
   });
   console.log('Deck Cleared');
 }
 
-function deal(){
-  var myEle = document.querySelector('.card');
-  if(myEle){
-    console.log('Error, deck has already been dealt');
-  } else {
-    for (var i = 0; i < cards.length; i++) {
-      for (var j = 0; j < suits.length; j++) {
-        //console.log(cards[i] + ' of ' + suits[j]);
-        var card = document.createElement('div');
-        card.setAttribute('class', 'card');
-        card.appendChild(document.createTextNode(cards[i] + ' of ' + suits[j]));
-        document.querySelector('.card-container').appendChild(card);
-      }
-    }
-    console.log('Cards dealt');
+function drawCard(){
+  var random = Math.floor(Math.random() * (mixer.length + 1));
+  console.log(random);
+  //add to drawnCards
+  drawnCards.splice(0, 0, mixer[random]);
+  //remove from mixer
+  mixer.splice(mixer[random], 1);
+  //remove all drawn cards to prevent duplicates being shown
+  cardElem = document.querySelectorAll('.drawn-card');
+  Array.prototype.forEach.call(cardElem, function(node) {
+    node.parentNode.removeChild(node);
+  });
+  //display drawn cards in DOM
+  for (var i = 0; i < drawnCards.length; i++) {
+    var drawnCard = document.createElement('div');
+    drawnCard.setAttribute('class', 'drawn-card');
+    drawnCard.appendChild(document.createTextNode(drawnCards[i]));
+    document.querySelector('.table-container').appendChild(drawnCard);
+  }
+  updateCards();
+}
+
+function updateCards(){
+  cardElem = document.querySelectorAll('.card');
+  Array.prototype.forEach.call(cardElem, function(node) {
+    node.parentNode.removeChild(node);
+  });
+  for (var i = 0; i < mixer.length; i++) {
+    var card = document.createElement('div');
+    card.setAttribute('class', 'card');
+    card.appendChild(document.createTextNode(mixer[i]));
+    document.querySelector('.card-container').appendChild(card);
+  }
+}
+
+function sortBy(){
+  var selectBox = document.querySelector("#selectBox");
+  var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+  if (selectedValue === 'Suit') {
+    console.log('Sort By Suit');
+    drawnCards.sort();
+  }
+  if (selectedValue === 'Value') {
+    console.log('Sort By Value');
   }
 }
